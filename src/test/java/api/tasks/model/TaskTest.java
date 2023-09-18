e9c9a77c2d3e975be27605c3947d6f613fd66898
chadrakh <chadrak@outlook.com>
2023-09-18 20:39:45 +0100

    Chore: Add Test Dependencies

--- src/test/java/api/tasks/model/TaskTest.java
+++ src/test/java/api/tasks/model/TaskTest.java
@@ -1,6 +1,15 @@
 package api.tasks.model;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TaskTest {
-  
+    @Disabled
+    @Test
+    public void testTaskInitialisation() {
+
+    }
+
 }
--- src/test/java/api/tasks/model/dto/TaskDTOTest.java
+++ src/test/java/api/tasks/model/dto/TaskDTOTest.java
@@ -1,6 +1,15 @@
 package api.tasks.model.dto;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TaskDTOTest {
-  
+    @Disabled
+    @Test
+    public void testTaskDTOInitialisation() {
+
+    }
+
 }
