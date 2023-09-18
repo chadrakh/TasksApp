1ff8211159bddc13427e7f053ec0882f19c337f2
chadrakh <chadrak@outlook.com>
2023-09-18 16:47:43 +0100

    Chore: Add Spring Boot Starter Code

---
+++ src/main/java/api/tasks/TasksAppApplication.java
@@ -0,0 +1,13 @@
+package api.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksAppApplication.class, args);
+    }
+
+}
