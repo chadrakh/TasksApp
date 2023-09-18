b28ec7c792d66a114848af2957200fdc8098338e
chadrakh <chadrak@outlook.com>
2023-09-18 17:54:25 +0100

    Feat: Add TaskFrequency enumeration

---
+++ src/main/java/api/tasks/model/DTO/TaskDTO.java
@@ -0,0 +1,2 @@
+package api.tasks.model.DTO;public class TaskDTO {
+}
---
+++ src/main/java/api/tasks/model/TaskFrequency.java
@@ -0,0 +1,10 @@
+package api.tasks.model;
+
+public enum TaskFrequency {
+    NEVER,
+    HOURLY,
+    DAILY,
+    WEEKLY,
+    BIWEEKLY,
+    MONTHLY
+}
