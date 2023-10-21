1be0fe0c1c019ae691b222b44e8355cf3f09ec4d
chadrakh <chadrak@outlook.com>
2023-10-22 00:31:43 +0100

    Feat: Add Tasks GET Mapping

--- src/main/java/com/tasksapp/controller/TaskController.java
+++ src/main/java/com/tasksapp/controller/TaskController.java
@@ -1,9 +1,32 @@
 package com.tasksapp.controller;
 
+import com.tasksapp.model.Task;
+import com.tasksapp.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/task")
+@RequestMapping("/api/v1/")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> getTasks() {
+        List<Task> tasks = taskService.getTasks();
+
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
 }
