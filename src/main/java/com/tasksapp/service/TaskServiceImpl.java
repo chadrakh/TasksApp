86ed30ef6764c7fcb1fcea59702b0ac6042d9cbb
chadrakh <chadrak@outlook.com>
2023-10-21 22:06:53 +0100

    Feat: Add TaskService Impl Method Overriding

--- src/main/java/com/tasksapp/service/TaskServiceImpl.java
+++ src/main/java/com/tasksapp/service/TaskServiceImpl.java
@@ -1,4 +1,48 @@
 package com.tasksapp.service;
 
-public class TaskServiceImpl {
+import com.tasksapp.model.Task;
+import com.tasksapp.model.TaskStatus;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public List<Task> getTasks() {
+        return null;
+    }
+
+    @Override
+    public List<Task> getTasksById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getTasksByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getTasksByStatus(TaskStatus status) {
+        return null;
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        return null;
+    }
+
+    @Override
+    public boolean updateTask(Task task) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteTaskById(Long id) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteTaskByTitle(String title) {
+        return false;
+    }
 }
