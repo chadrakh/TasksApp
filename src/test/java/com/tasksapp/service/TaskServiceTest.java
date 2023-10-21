b64f1477ba992a58236004c8ff1d943de3b40562
chadrakh <chadrak@outlook.com>
2023-10-21 21:49:50 +0100

    Test: Create TaskService Test

---
+++ src/test/java/com/tasksapp/service/TaskServiceTest.java
@@ -0,0 +1,11 @@
+package com.tasksapp.service;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@DataJpaTest
+class TaskServiceTest {
+
+}
